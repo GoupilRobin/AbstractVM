50298454a97eb599a7b22311ffa7d92442155d38
JamesDev <dev.james.d@gmail.com>
2013-02-14 19:32:45 +0100

    forgot the modified main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,24 @@
 // Login   <goupil_r@epitech.net>
 //
 // Started on  Wed Feb 13 10:47:40 2013 robin goupil
-// Last update Wed Feb 13 14:38:29 2013 robin goupil
+// Last update Thu Feb 14 19:31:49 2013 robin goupil
 //
 
+#include <iostream>
 #include "IOperand.hpp"
-#include "TOperand.hpp"
+#include "Operand.hpp"
 
 int		main(int ac, char **av)
 {
+  Operand		a(Int8, 100.3);
+  Operand		b(Double, 127);
+  IOperand		*res;
+
   (void) ac;
   (void) av;
+  res = (a + b);
+  std::cout << "(dynamic) a + b = " << *(a + b) << std::endl;
+  std::cout << "(stored) a + b = " << *res << std::endl;
+  std::cout << "(dynamic) x + y = " << *(Operand(Int8, 1) + Operand(Float, 3.14)) << std::endl;
   return (0);
 }
