2e69b3dc6f378a259eb8329fb46a62fb763a4cfa
JamesDev <dev.james.d@gmail.com>
2013-02-22 18:04:52 +0100

    fix

--- Parse.hpp
+++ Parse.hpp
@@ -1,11 +1,11 @@
 //
 // Parse.hpp for AVM in /home/chirou_t//tek2/projets/c++/abstract/AbstractVM
-// 
+//
 // Made by thomas chiroussot-chambeaux
 // Login   <chirou_t@epitech.net>
-// 
+//
 // Started on  Fri Feb 15 15:29:04 2013 thomas chiroussot-chambeaux
-// Last update Fri Feb 22 17:54:51 2013 thomas chiroussot-chambeaux
+// Last update Fri Feb 22 18:04:38 2013 robin goupil
 //
 
 #ifndef __PARSE__
@@ -14,13 +14,12 @@
 
 #include <string>
 #include <list>
-#include "Call.hpp"
 
 class	Parse
 {
 private:
   std::list<std::string> _list;
-  func_list _struct;
+
 public:
   Parse();
   ~Parse();
